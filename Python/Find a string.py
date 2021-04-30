ec23c6af720abf9f69aac4a1d0c28291ca800c9f
AntoanStefanov <antoine.stefanov@gmail.com>
2021-04-30 11:14:42 +0300

    Find a string

---
+++ Python/Find a string.py
@@ -0,0 +1,16 @@
+def count_substring(string, sub_string):
+    length = len(sub_string)
+    count = 0
+    for i in range(len(string)):
+        if string[i] == sub_string[0]:
+            if string[i: i+length] == sub_string:
+                count += 1
+    return count
+
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+
+    count = count_substring(string, sub_string)
+    print(count)

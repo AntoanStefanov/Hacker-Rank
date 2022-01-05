e9872b8395e873fa1b87295f16ff4bf76135cbdb
AntoanStefanov <antoine.stefanov@gmail.com>
2022-01-05 22:40:20 +0200

    another way solving it

--- Python/String Validators.py
+++ Python/String Validators.py
@@ -1,4 +1,7 @@
 s = input()
+# x = (c.isalnum() for c in s)
+# could be done with generator expressions
+# any(c.isalnum() for c in s), no [], but ()
 print(any([c.isalnum() for c in s]))
 print(any([c.isalpha() for c in s]))
 print(any([c.isdigit() for c in s]))
